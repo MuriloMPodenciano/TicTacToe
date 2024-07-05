37707658766cd3835a7e8687c2dffe1b802a2f8c
MuriloMPodenciano <murilo.m.podenciano@gmail.com>
2024-07-05 05:09:56 -0300

    refactor: moved id generation to dao and added alert for entrar button

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -2,14 +2,18 @@ package com.example.tictactoe
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tictactoe.Constant.EXTRA_JOGO
 import com.example.tictactoe.databinding.ActivityMainBinding
-import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val jogoDao: JogoDAO by lazy {
+        JogoDAOFirebase()
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,13 +26,10 @@ class MainActivity : AppCompatActivity() {
 
         amb.criarBt.setOnClickListener {
             val intent = Intent(this, JogoActivity::class.java)
-
-            val msg = Jogo(
-                id = generateId()
-            )
+            val id = jogoDao.createJogo()
 
             val resultIntent = Intent()
-            resultIntent.putExtra(EXTRA_JOGO, msg)
+            resultIntent.putExtra(EXTRA_JOGO, id)
             setResult(RESULT_OK, resultIntent)
 
             startActivity(intent)
@@ -36,10 +37,15 @@ class MainActivity : AppCompatActivity() {
 
         amb.entrarBt.setOnClickListener {
             val intent = Intent(this, JogoActivity::class.java)
+
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            builder.setTitle("Insira um ID existente")
+
+            val resultIntent = Intent()
+            resultIntent.putExtra(EXTRA_JOGO, id)
+            setResult(RESULT_OK, resultIntent)
+
             startActivity(intent)
         }
-    }
-    private fun generateId(): Int{
-        return Random(System.currentTimeMillis()).nextInt()
     }
 }
