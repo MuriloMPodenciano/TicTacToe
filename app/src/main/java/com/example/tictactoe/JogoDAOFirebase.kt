80f4034d532ba8582dc829b4f5bf1519c7c0be8f
MuriloMPodenciano <murilo.m.podenciano@gmail.com>
2024-07-05 05:13:59 -0300

    feat: created DAO for firebase

--- app/src/main/java/com/example/tictactoe/JogoDAOFirebase.kt
+++ app/src/main/java/com/example/tictactoe/JogoDAOFirebase.kt
@@ -1,4 +1,30 @@
 package com.example.tictactoe
 
-class JogoDAOFb {
+import com.google.firebase.Firebase
+import com.google.firebase.database.database
+import kotlin.random.Random
+
+class JogoDAOFirebase : JogoDAO{
+    companion object{
+        private const val JOGO_ROOT_NODE = "jogo"
+    }
+
+    private val jogoFirebaseReference = Firebase.database.getReference(JOGO_ROOT_NODE)
+
+    override fun createJogo(): Int {
+        val newJogo = Jogo(
+            id = generateId()
+        )
+        jogoFirebaseReference.child(newJogo.id.toString()).setValue(newJogo)
+
+        return newJogo.id
+    }
+
+    override fun findJogoById(id: Int) {
+
+    }
+
+    private fun generateId(): Int{
+        return Random(System.currentTimeMillis()).nextInt()
+    }
 }
