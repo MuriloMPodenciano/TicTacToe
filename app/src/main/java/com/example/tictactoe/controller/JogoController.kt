f6fe869342849663f6f47fef2c13a071d7e51931
MuriloMPodenciano <murilo.m.podenciano@gmail.com>
2024-07-05 11:57:12 -0300

    refactor: removed negative values from ids

--- app/src/main/java/com/example/tictactoe/controller/JogoController.kt
+++ app/src/main/java/com/example/tictactoe/controller/JogoController.kt
@@ -3,6 +3,7 @@ package com.example.tictactoe.controller
 import com.example.tictactoe.model.Jogo
 import com.example.tictactoe.dao.JogoDAO
 import com.example.tictactoe.dao.JogoDAOFirebase
+import kotlin.math.abs
 import kotlin.random.Random
 
 class JogoController {
@@ -11,7 +12,7 @@ class JogoController {
         JogoDAOFirebase()
     }
     private fun generateId(): String{
-        return Random(System.currentTimeMillis()).nextInt().toString()
+        return abs(Random(System.currentTimeMillis()).nextInt()).toString()
     }
 
     fun createJogo(): String{
