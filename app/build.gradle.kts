b6cc76f4cabdf31c6106019a2a4e4ecf128cd86a
MuriloMPodenciano <murilo.m.podenciano@gmail.com>
2024-07-05 03:59:32 -0300

    fix: fixed firebase dependencies on build gradle again

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ android {
 
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
+    implementation("com.google.firebase:firebase-database:21.0.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
