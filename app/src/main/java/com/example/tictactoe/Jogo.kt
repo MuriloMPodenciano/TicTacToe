2ce399893dd45c52261179a0ead3a34a979b701d
MuriloMPodenciano <murilo.m.podenciano@gmail.com>
2024-07-05 04:50:50 -0300

    refactor: added enum for diferent positions

--- app/src/main/java/com/example/tictactoe/Jogo.kt
+++ app/src/main/java/com/example/tictactoe/Jogo.kt
@@ -6,4 +6,13 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class Jogo(
     var id: Int = -1,
-): Parcelable
+    var a1: Posicao = Posicao.VAZIO,
+    var a2: Posicao = Posicao.VAZIO,
+    var a3: Posicao = Posicao.VAZIO,
+    var b1: Posicao = Posicao.VAZIO,
+    var b2: Posicao = Posicao.VAZIO,
+    var b3: Posicao = Posicao.VAZIO,
+    var c1: Posicao = Posicao.VAZIO,
+    var c2: Posicao = Posicao.VAZIO,
+    var c3: Posicao = Posicao.VAZIO,
+    ): Parcelable
