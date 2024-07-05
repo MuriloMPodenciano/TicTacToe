9075d3dbcbd8404c287392f3c6a050f013a99d77
MuriloMPodenciano <murilo.m.podenciano@gmail.com>
2024-07-05 11:00:43 -0300

    refactor: made jogo use list instead of variables

--- app/src/main/java/com/example/tictactoe/model/Jogo.kt
+++ app/src/main/java/com/example/tictactoe/model/Jogo.kt
@@ -5,15 +5,7 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Jogo(
-    var jogador: Int = 1,
     var id: String = "",
-    var a1: Posicao = Posicao.VAZIO,
-    var a2: Posicao = Posicao.VAZIO,
-    var a3: Posicao = Posicao.VAZIO,
-    var b1: Posicao = Posicao.VAZIO,
-    var b2: Posicao = Posicao.VAZIO,
-    var b3: Posicao = Posicao.VAZIO,
-    var c1: Posicao = Posicao.VAZIO,
-    var c2: Posicao = Posicao.VAZIO,
-    var c3: Posicao = Posicao.VAZIO,
+    var table: MutableList<Posicao> = mutableListOf(Posicao.VAZIO,Posicao.VAZIO,Posicao.VAZIO,Posicao.VAZIO,Posicao.VAZIO,Posicao.VAZIO,Posicao.VAZIO,Posicao.VAZIO,Posicao.VAZIO),
+    var jogador: Int = 1
     ): Parcelable
